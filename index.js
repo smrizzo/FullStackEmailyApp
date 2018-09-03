d852fe9f09cc5df5141708f8f88ee7acddb3313f
Scott Rizzo <jkarizzo345@yahoo.com>
2018-09-03 16:13:58 -0700

    finished auth flow

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/user');
+require('./services/passport');
+
+
 const app = express();
+//some cookie middleware
+app.use(
+   cookieSession({
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+   })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
 
+authRoutes(app);
+//could use equire('./routes/authRoutes')(app);
 
-app.get('/', (req, res) => {
-   res.send({ name: 'skywalker' });
-});
-
+mongoose.connect(keys.mongoURI);
 
 const port = process.env.PORT || 5000;
 app.listen(port);
