12380a7e517c05971c77b7e9959487ad8fd4966e
Scott Rizzo <jkarizzo345@yahoo.com>
2018-09-10 17:26:01 -0700

    index.js file changed

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ require('./services/passport');
 const app = express();
 //some cookie middleware
 
+if(process.env.NODE_ENV === 'production') {
+   //Express will serve up production issues
+   //like our main.js file, or main.css file
+
+   app.use(express.static('client/build'));
+
+   //Express will serve up our index.html if it doesnt recognize the route thats being requested
+   const path = require('path');
+   app.get('*', (req, res) => {
+      res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+
+}
+
 app.use(bodyParser.json());
 
 app.use(
@@ -27,6 +41,8 @@ app.use(passport.session());
 authRoutes(app);
 //could use equire('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+
 
 mongoose.connect(keys.mongoURI);
 
